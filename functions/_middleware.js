7aec38861ae9553ee5afd3e799ca4e413d964559
Ewgenoff <Ewgenoff30@gmail.com>
2026-08-01 14:31:07 +0500

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,22 @@
+export async function onRequest(context) {
+  const authHeader = context.request.headers.get('Authorization');
+  if (authHeader) {
+    const [scheme, encoded] = authHeader.split(' ');
+    if (scheme === 'Basic') {
+      const decoded = atob(encoded);
+      const [user, pass] = decoded.split(':');
+      
+      // УКАЖИТЕ СВОЙ ЛОГИН И ПАРОЛЬ:
+      if (user === 'Ewgenoff' && pass === '663770+') {
+        return await context.next();
+      }
+    }
+  }
+
+  return new Response('Требуется авторизация', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Protected Area"',
+    },
+  });
+}
